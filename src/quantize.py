77770703a03a210e38a9eb22c97c09666309d1c2
Karthik Kumar G <56603114+KarthikKumarKT@users.noreply.github.com>
2025-08-02 06:30:37 +0530

    Fix CI: for Docker Prediction 7

--- src/quantize.py
+++ src/quantize.py
@@ -21,12 +21,12 @@ def dequantize_params(quantized, scale, min_val):
 
 def main():
     print("Loading model...")
-    model = joblib.load("/artifacts/linear_model.joblib")
+    model = joblib.load("../artifacts/linear_model.joblib")
     coef = model.coef_
     intercept = model.intercept_
 
     print("Saving unquantized parameters...")
-    os.makedirs("../artifacts", exist_ok=True)
+    os.makedirs("/artifacts", exist_ok=True)
     joblib.dump({'coef': coef, 'intercept': intercept}, "../artifacts/params_unquantized.joblib")
 
     print("Quantizing parameters...")
